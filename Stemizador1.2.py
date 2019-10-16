d404af7eed058e6b59537d2e7bba89380d60a26a
Breno Felix de Souza <brenodml@live.com>
2019-10-16 00:22:08 -0300

    Revert "Identify the call - comment"

--- Stemizador1.2.py
+++ Stemizador1.2.py
@@ -118,7 +118,6 @@ tam = len(palavra)
 PluralReduction()
 wordEndA = True if (palavra[tam - 1] == 'a') else False
 if(wordEndA):
-    #calling feminine function to apply all those laws.
     feminineReduction()
 AugmentativeReduction()
 AdverbReduction()
